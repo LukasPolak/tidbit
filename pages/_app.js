8faf481c3abe2d3e213135eb8ba7329254d71a87
Lukas Polak <lukash.polak@gmail.com>
2021-02-20 12:33:51 +0100

    feat(plausible): initialize plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import Head from 'next/head'
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script
+          async
+          defer
+          data-domain="tidbit.dev"
+          src="https://plausible.io/js/plausible.js"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
